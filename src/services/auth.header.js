29ddd52adff992ecb18c867a65a73303b33aaf0c
xnatsuk <xnatsuk@gmail.com>
2022-08-04 18:31:30 -0300

    :ambulance: Fix token not found error

--- src/services/auth.header.js
+++ src/services/auth.header.js
@@ -1,10 +1,10 @@
-import { local } from '@/services/auth.service';
+import { local } from '@/services/local.service';
 
 export default function authHeader() {
   const user = local.value.data;
 
   if (user && user.token) {
-    return { Authorization: `Bearer ${user.token}` };
+    return `Bearer ${user.token}`;
   }
   return {};
 }
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,19 +1,22 @@
 import axios from 'axios';
 
-import { local } from '@/services/auth.service';
+import { local } from '@/services/local.service';
 import authHeader from './auth.header';
 
-const API_URL = import.meta.env.VITE_API_URL;
-const config = { headers: authHeader() };
+// const API_URL = import.meta.env.VITE_API_URL;
+const id = local.value.data.id;
+
+axios.defaults.headers.common['Authorization'] = authHeader();
 
 class UserService {
-  incrementUserEntries() {
-    const email = local.value.data.email;
-    return axios.put(`/api/score`, { email }, config);
+  async incrementUserEntries() {
+    return await axios.put(`/api/score/${id}`);
   }
 
-  getUserScore() {
-    return axios.get(`/api/score`, config);
+  async getUserScore() {
+    return await axios.get(`/api/score/${id}`).then((response) => {
+      local.value.data.entries = response.data;
+    });
   }
 }
 export default new UserService();
